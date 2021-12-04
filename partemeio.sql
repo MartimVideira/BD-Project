a47eea51666f3c21256a984c37bc0cf81e2c7110
Martim <mitram790@gmail.com>
2021-12-04 18:00:03 +0000

    Added Mariana's work

--- partemeio.sql
+++ partemeio.sql
@@ -1,17 +1,17 @@
--- Match(idMatch ,startTime,duration, endTime, game -> Game,, address -> Address ) 
+DROP TABLE IF EXISTS Match
 CREATE TABLE Match (
     matchId INT  NOT NULL PRIMARY KEY,
     startTime DATE,
     duration  DATE,
     endTime DATE CONSTRAINT endTime = startTime + duration ,
     gameId INT  FOREIGN KEY REFERENCES Game(gameId),
-    addressId INT FOREIGN KEY REFERENCES  Address(addressId)S
+    addressId INT FOREIGN KEY REFERENCES  Address(addressId)
 
 );
 
 
 
---Game(gameId,name , typeOfGame) 
+DROP TABLE IF EXISTS Game 
 CREATE TABLE Game(
     gameId INT NOT NULL PRIMARY KEY ,
     name TEXT NOT NULL,
@@ -19,10 +19,32 @@ CREATE TABLE Game(
 );
 
 
---Participation(team ->team , match -> match , prize , classification)
+DROP TABLE IF EXISTS Participation
 CREATE TABLE Participation(
     prize FLOAT DEFAULT 0.0,
     classification TEXT,
     teamId INT FOREIGN KEY Team(teamId), 
     matchId INT FOREIGN KEY Match(matchId)
-    );
+);
+
+CREATE TABLE WorkedTime(
+    person INT NOT NULL PRIMARY KEY FOREIGN KEY REFERENCES Person(NIF),
+    match INT NOT NULL PRIMARY KEY FOREIGN KEY REFERENCES Match(matchId),
+    workedTime INT CONSTRAINT workedTime >= 0
+);
+
+
+DROP TABLE IF EXISTS GameFan;
+CREATE TABLE GameFan(
+    fan INT NOT NULL FOREIGN KEY REFERENCES Person(NIF),
+    game INT FOREIGN KEY REFERENCES Game(gameId),
+    PRIMARY KEY (fan,game)
+);
+
+
+DROP TABLE IF EXISTS TeamFan;
+CREATE TABLE TeamFan(
+    fan INT NOT NULL FOREIGN KEY REFERENCES Person(NIF),
+    team INT FOREIGN KEY REFERENCES Team(idTeam),
+    PRIMARY KEY (fan,team)
+);
