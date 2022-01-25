96181c7027dfa40f6a0c86cd5a2c7181907e3e68
df00112 <df00112@hotmail.com>
2022-01-25 17:44:58 +0000

    gatilho2

--- gatilho2_adiciona.sql
+++ gatilho2_adiciona.sql
@@ -6,7 +6,8 @@
 
 --verifica, se o tempo introduzido é válido (maior ou igual a 0) antes da inserção
 Create Trigger durationCheckBeforeInsert Before Insert on Match
-When (new.duration < '00:00')
+FOR EACH ROW
+When (strftime('%H',new.duration) IS NULL OR strftime('%M',new.duration) IS NULL OR strftime('%S',new.duration) IS NULL)
 Begin
     Select raise(ABORT, 'Existe uma duracao invalida');
 End;
@@ -14,13 +15,21 @@ End;
 --insert, depois de inserir, o endtime deve aparecer como soma de starttime e duration
 CREATE TRIGGER addEndTime 
 AFTER INSERT ON Match 
+FOR EACH ROW
 BEGIN 
     update match set endtime = time(strftime('%s',startTime) + strftime('%s',duration),'unixepoch');
 END;
 
 --verifica, se o tempo introduzido é válido (maior ou igual a 0) antes da atualização
 Create Trigger durationCheckBeforeUpdate Before update on Match
-When (new.duration < '00:00')
+When (strftime('%H',new.duration) IS NULL OR strftime('%M',new.duration) IS NULL OR strftime('%S',new.duration) IS NULL)
 Begin
     Select raise(ABORT, 'Existe uma duracao invalida');
 End;
+
+--update, depois de atualizar, o endtime deve aparecer como soma de starttime e duration
+CREATE TRIGGER updateEndTime 
+AFTER UPDATE ON Match 
+BEGIN 
+    update match set endtime = time(strftime('%s',startTime) + strftime('%s',duration),'unixepoch');
+END;
--- gatilho2_verifica.sql
+++ gatilho2_verifica.sql
@@ -3,18 +3,18 @@
 .nullvalue NULL
 
 .print ''
-.print 'validar o insert para um valor negativo (embora nao faca muito sentido)'
+.print 'validar o insert para um valor negativo 01:-20'
 .print ''
-.print 'deve aparecer 1 mensagem'
+.print 'deve aparecer 1 erro'
 .print ''
 
 .read criar.sql
 .read gatilho2_adiciona.sql
-Insert into match(starttime,duration,gameid,addressid) values('10:30','-01:30',2,15);
+Insert into match(starttime,duration,gameid,addressid) values('10:30','01:-20',2,15);
 .read gatilho2_remove.sql
 
 .print ''
-.print 'verificar insert - endtime = starttime + duration'
+.print 'VERIFICAR INSERT, endtime = starttime + duration'
 .print ''
 .print 'antes de ter trigger'
 .print ''
@@ -33,7 +33,7 @@ select * from match;
 select * from match;
 
 .print ''
-.print 'vamos inserir uma nova partida depois de termos o trigger'
+.print 'vamos inserir uma nova partida (matchId = 11) depois de termos o trigger'
 .print ''
 
 insert into match(starttime,duration,gameid,addressid) values ('08:00','00:30',1,1);
@@ -45,15 +45,24 @@ insert into match(starttime,duration,gameid,addressid) values ('08:00','00:30',1
 select * from match;
 
 .print ''
-.print 'caso introduza um valor incorreto, voltara a aparecer a mensagem'
+.print 'caso introduza um valor incorreto, voltara a aparecer 1 erro'
 .print ''
 
-Insert into match(starttime,duration,gameid,addressid) values('10:30','-01:30',2,15);
+Insert into match(starttime,duration,gameid,addressid) values('10:30','01:-30',2,15);
 
 .print ''
-.print 'validar o update para um valor negativo (embora nao faca muito sentido)'
+.print 'validar o update para um valor incorreto'
 .print ''
-.print 'deve aparecer 1 mensagem'
+.print 'deve aparecer 1 erro se for dar update com uma duration de -01:00'
 .print ''
 
-update match set duration = '-01:00';
+update match set duration = '-01:00' where matchId = 1;
+
+.print ''
+.print 'validar o update para um valor correto'
+.print ''
+.print 'update em duration da partida de id 11'
+.print ''
+
+update match set duration = '01:00' where matchId = 11;
+select * from match;
